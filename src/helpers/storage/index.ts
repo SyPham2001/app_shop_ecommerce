9f5497be1aee8fd55bb5ca4ecf8243e555711639
sydeptrai112 <phamsy113.spm@gmail.com>
2024-11-19 16:38:55 +0700

    update-loginInterceptor-axios

--- src/helpers/storage/index.ts
+++ src/helpers/storage/index.ts
@@ -1,4 +1,4 @@
-import { ACCESS_TOKEN, REFRESH_TOKEN, USER_DATA } from 'src/configs/auth'
+import { ACCESS_TOKEN, REFRESH_TOKEN, TEMPORARY_TOKEN, USER_DATA } from 'src/configs/auth'
 
 export const setLocalUserData = (userData: string, accessToken: string, refreshToken: string) => {
   if (typeof window !== 'undefined') {
@@ -29,5 +29,11 @@ export const clearLocalUserData = () => {
     window.localStorage.removeItem(USER_DATA)
     window.localStorage.removeItem(ACCESS_TOKEN)
     window.localStorage.removeItem(REFRESH_TOKEN)
+  }
+}
+
+export const clearTemporaryToken = () => {
+  if (typeof window !== 'undefined') {
+    window.localStorage.removeItem(TEMPORARY_TOKEN)
   }
 }
--- src/services/auth.ts
+++ src/services/auth.ts
@@ -2,13 +2,14 @@ import axios from 'axios'
 
 //* config
 import { CONFIG_API } from 'src/configs/api'
+import instanceAxios from 'src/helpers/axios'
 
 //** types */
 import { TLoginAuth } from 'src/types/auth'
 
 export const loginAuth = async (data: TLoginAuth) => {
   try {
-    const res = await axios.post(`${CONFIG_API.AUTH.INDEX}/login`, data)
+    const res = await instanceAxios.post(`${CONFIG_API.AUTH.INDEX}/login`, data)
     return res.data
   } catch (error) {
     return null
@@ -17,7 +18,7 @@ export const loginAuth = async (data: TLoginAuth) => {
 
 export const logoutAuth = async () => {
   try {
-    const res = await axios.post(`${CONFIG_API.AUTH.INDEX}/logout`)
+    const res = await instanceAxios.post(`${CONFIG_API.AUTH.INDEX}/logout`)
     return res.data
   } catch (error) {
     return null
